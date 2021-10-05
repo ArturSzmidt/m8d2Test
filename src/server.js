5281079f79809034b42ae6619ae9ad309fb6b088
ArturSzmidt <80460700+ArturSzmidt@users.noreply.github.com>
2021-10-05 16:27:51 +0200

    ++

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-import express from "express";
-import cors from "cors";
-import productsRouter from "./services/products/index.js";
+import express from 'express';
+import cors from 'cors';
+import productsRouter from './services/products/index.js';
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get("/test", (req, res) => {
-    res.status(200).send({ message: "Test success" })
-})
+app.get('/test', (req, res) => {
+  res.status(200).send({ message: 'Test success' });
+});
 
-app.use("/products", productsRouter)
+app.use('/products', productsRouter);
 
-export default app
+export default app;
--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,26 +1,36 @@
-import express from "express"
-import ProductModel from "./model.js"
+import express from 'express';
+import ProductModel from './model.js';
 
-const productsRouter = express.Router()
+const productsRouter = express.Router();
+
+productsRouter.get('/', async (req, res) => {
+  try {
+    const products = await ProductModel.find();
+
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 productsRouter.post('/', async (req, res) => {
-    // create a new product.
-    try {
-        const newProduct = new ProductModel(req.body)
-        await newProduct.save()
-        res.status(201).send(newProduct)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
+  // create a new product.
+  try {
+    const newProduct = new ProductModel(req.body);
+    await newProduct.save();
+    res.status(201).send(newProduct);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 productsRouter.get('/:id', async (req, res) => {
-    try {
-        const product = await ProductModel.findById(req.params.id)
-        res.status(200).send(product)
-    } catch (error) {
-        res.status(404).send(error)
-    }
-})
+  try {
+    const product = await ProductModel.findById(req.params.id);
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
 
-export default productsRouter
+export default productsRouter;
